7ce6f69334f73c0075973081555a8346645af765
j_n <tupiszakaczki@gmail.com>
2016-03-13 11:13:21 +0100

    init

---
+++ R/punkty_ndvi.R
@@ -0,0 +1,11 @@
+#' @name punkty_ndvi
+#' @title Pollen count of Alnus, Betula, and Corylus in given polish sites
+#'
+#' @description aerobiologia contain data about pollen count of  Alnus, Betula, 
+#' and Corylus in given polish sites (without incomplete data from Gdańsk in years 1998 and 1999)
+#' 
+#' @docType data
+#'
+#' @format data.frame 
+
+NULL
